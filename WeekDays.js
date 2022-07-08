0d06850613866d8c5134bff9098cee7aa26ab04d
Sangram <sangram15.behera@gmail.com>
2022-07-08 06:49:58 +0530

    Week Days

---
+++ WeekDays.js
@@ -0,0 +1,24 @@
+const prompt = require("prompt-sync")();
+var number = prompt("Please enter the number between 1 to 7 :");
+
+if (number == 1) {
+    console.log("Monday"); 
+} 
+else if (number == 2){
+    console.log("Tuesday");
+}
+else if (number == 3) {
+    console.log("Wednesday"); 
+} 
+else if (number == 4){
+    console.log("Thursday");
+}
+else if (number == 5) {
+    console.log("Friday"); 
+} 
+else if (number == 6){
+    console.log("Saturday");
+}
+else{
+    console.log("Sunday"); 
+}
